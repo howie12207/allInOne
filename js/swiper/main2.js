1793ae6c374798540443f4faf8ad910e63806d32
Fanzai <fan11204@gmail.com>
2023-12-05 10:34:18 +0800

    feat: for test

--- js/swiper/main2.js
+++ js/swiper/main2.js
@@ -5,12 +5,12 @@ new Vue({
             activeTab: 0,
         };
     },
-    watch: {
-        activeTab(newVal) {
-            this.$refs.tabSwiper.swiper.slideTo(newVal);
-            this.$refs.mainSwiper.swiper.slideTo(newVal);
-        },
-    },
+    // watch: {
+    //     activeTab(newVal) {
+    //         this.$refs.tabSwiper.swiper.slideTo(newVal);
+    //         this.$refs.mainSwiper.swiper.slideTo(newVal);
+    //     },
+    // },
     mounted() {
         this.$nextTick(() => {
             new Swiper('.main-swiper', {
@@ -19,26 +19,27 @@ new Vue({
                 autoplay: false,
             });
             new Swiper('.tab-swiper', {
-                perPageView: 2,
+                slidesPerView: 2,
                 loop: true,
                 autoplay: true,
                 speed: 500,
+                slideToClickedSlide: true,
                 pagination: {
                     el: '.swiper-pagination',
                     clickable: true,
                 },
-                breakpoints: {
-                    // when window width is >= 320px
-                    320: {
-                        slidesPerView: 2,
-                        spaceBetween: 20,
-                    },
-                    // when window width is >= 640px
-                    768: {
-                        slidesPerView: 4,
-                        spaceBetween: 20,
-                    },
-                },
+                // breakpoints: {
+                //     // when window width is >= 320px
+                //     320: {
+                //         slidesPerView: 2,
+                //         spaceBetween: 20,
+                //     },
+                //     // when window width is >= 640px
+                //     768: {
+                //         slidesPerView: 4,
+                //         spaceBetween: 20,
+                //     },
+                // },
             });
             this.$refs.tabSwiper.swiper.on('slideChange', this.handleTabSlideChange);
             this.$refs.mainSwiper.swiper.on('slideChange', this.handleSlideChange);
@@ -46,7 +47,9 @@ new Vue({
     },
     methods: {
         handleTabClick(index) {
+            console.log(index);
             this.activeTab = index;
+            // this.$refs.tabSwiper.swiper.slideTo(index);
         },
         handleTabSlideChange() {
             this.$nextTick(() => {
